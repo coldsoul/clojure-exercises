3ec6eb5b97336931b03e1d4cc5dd63f4e54c4751
Radi Radichev <radi.radichev@gmail.com>
2014-06-01 19:15:44 +0100

    Add some edge cases to tests, update gitignore to ignore .nrepl-port

--- test/exercises/tests_basic.clj
+++ test/exercises/tests_basic.clj
@@ -4,10 +4,13 @@
 
 (fact "second-nth returns the second element of a list"
       (basic/second-nth '(1 2 3 4)) => 2
-      (basic/second-nth '(2 3 4)) => 3)
+      (basic/second-nth '(2 3 4)) => 3
+      (basic/second-nth '(2)) => (throws IndexOutOfBoundsException))
 
 (fact "third returns the third element of a list"
-      (basic/third '(1 2 3 4 5)) => 3)
+      (basic/third '(1 2 3 4 5)) => 3
+      (basic/third '(1 2)) => (throws IndexOutOfBoundsException))
 
 (fact "third-lst returns the third element of a list"
-      (basic/third-lst '(1 2 3 4 5)) => 3)
+      (basic/third-lst '(1 2 3 4 5)) => 3
+      (basic/third-lst '(2 3)) => nil)
